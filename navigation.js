39a2cacff8601525597fd135e1465a340f5d955b
Elaine Wang <elainewang.cal@gmail.com>
2020-03-02 19:07:19 -0800

    Initial setup

---
+++ navigation.js
@@ -0,0 +1,43 @@
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Navigation = function (_React$Component) {
+  _inherits(Navigation, _React$Component);
+
+  function Navigation() {
+    _classCallCheck(this, Navigation);
+
+    return _possibleConstructorReturn(this, (Navigation.__proto__ || Object.getPrototypeOf(Navigation)).apply(this, arguments));
+  }
+
+  _createClass(Navigation, [{
+    key: "render",
+    value: function render() {
+      var testCondition = true;
+
+      // var ourRequest = new XMLHttpRequest();
+      // ourRequest.open('GET', 'https://raw.githubusercontent.com/elainedev/retailNav/master/navigation.json');
+      // ourRequest.onload = function() {
+      //   console.log('fucking ifnally', ourRequest.responseText);
+      // }
+      // ourRequest.send();
+
+      return React.createElement(
+        "div",
+        { className: "parent" },
+        "new test"
+      );
+    }
+  }]);
+
+  return Navigation;
+}(React.Component);
+
+var domContainer = document.querySelector("#navigation-menu");
+
+ReactDOM.render(React.createElement(Navigation, null), domContainer);
---
+++ src/navigation.jsx
@@ -0,0 +1,30 @@
+
+class Navigation extends React.Component {
+
+
+
+  render() {
+    const testCondition = true;
+
+    // var ourRequest = new XMLHttpRequest();
+    // ourRequest.open('GET', 'https://raw.githubusercontent.com/elainedev/retailNav/master/navigation.json');
+    // ourRequest.onload = function() {
+    //   console.log('fucking ifnally', ourRequest.responseText);
+    // }
+    // ourRequest.send();
+
+    return (
+      <div className="parent">
+        
+          new test
+      </div>
+
+    )
+  }
+}
+
+
+
+const domContainer = document.querySelector("#navigation-menu");
+
+ReactDOM.render(<Navigation />, domContainer);
